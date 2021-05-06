f06aeb9aa14c53c9ca636cee965341e51bed1f56
lauren_valles <lvalles10@g.ucla.edu>
2021-05-06 13:07:31 -0700

    commit

---
+++ Week_05/js/init.js
@@ -0,0 +1,41 @@
+const map = L.map('map').setView([34.0709, -118.444], 5);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+function addMarker(data){
+        // console.log(data)
+        // these are the names of our fields in the google sheets:
+        L.marker([data.lat,data.lng]).addTo(map).bindPopup(`<h3>${data.Location}</h3>`)
+        return data.Timestamp
+}
+
+let url = "https://spreadsheets.google.com/feeds/list/18UGaqNA_XlhgrzZs-cpZd8pUdWXg0f-MeslJ9L9YwXc/on609jc/public/values?alt=json"
+
+fetch(url)
+	.then(response => {
+		return response.json();
+		})
+    .then(data =>{
+                // console.log(data)
+                formatData(data)
+        }
+)
+
+
+function formatData(theData){
+        const formattedData = [] /* this array will eventually be populated with the contents of the spreadsheet's rows */
+        const rows = theData.feed.entry
+        for(const row of rows) {
+          const formattedRow = {}
+          for(const key in row) {
+            if(key.startsWith("gsx$")) {
+                  formattedRow[key.replace("gsx$", "")] = row[key].$t
+            }
+          }
+          formattedData.push(formattedRow)
+        }
+        console.log(formattedData)
+        formattedData.forEach(addMarker)
+}
